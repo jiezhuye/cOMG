6cf5f1bc8de9dfdb5c8ac7033f9b8270f9ddca86
Scelta <scelta21st@gmail.com>
2016-01-20 23:24:29 +0800

    failed to run paralleled. And unable to run as a huge table that the RAM would reaching 200G.

--- genomics/bin/downsize_Random.pl
+++ genomics/bin/downsize_Random.pl
@@ -4,8 +4,9 @@ use strict;
 die &usage unless @ARGV == 4;
 
 my(%total,%id,%lengths,%abundance,%totalabundance,%totalreads);
-my($if,$it,$len,$out) = @ARGV;
-my $targetSize; # The # of reads u wanna trim to.
+#my($if,$it,$len,$out) = @ARGV;
+my($targetSize,$it,$len,$out) = @ARGV;
+#my $targetSize; # The # of reads u wanna trim to.
 my $gss = 0; # gene set size
 =cut
 open I,"$if" or die "$!\n";
@@ -17,6 +18,7 @@ while(<I>)
 }
 close I;
 =cut
+open ST,">$out.stat" || die "Cant open $out.stat. ".$!;
 open I,"$len" or die "$!\n";
 while(<I>)
 {
@@ -33,11 +35,12 @@ open I,"$it" or die "$!\n";
 my $name=(split /\./,$it)[0];
 my $basename=(split /\//,$name)[-1];
 my $head = <I>; chomp($head);
-my @heads = split($head);
+my @heads = split(/\t/,$head);
 while(<I>)
 {
     chomp;
     my @temp=split;
+
 	for (my $i=1;$i<@temp;$i++){
 	    $id{$heads[$i]}{$temp[0]}=$temp[$i];
 		$total{$heads[$i]} += $temp[$i];
@@ -45,43 +48,52 @@ while(<I>)
 }
 close I;
 
-for (my $i=1;$i<@heads;$i++){
-	my $time=$total{$heads[$i]}-$targetSize;
+for (my $h=1;$h<@heads;$h++){
+	my $time=$total{$heads[$h]}-$targetSize;
+	if ($time < 0){
+		print ST "$heads[$h] is less than $targetSize. Discarded.\n";
+		splice @heads,$h,1;
+		$h --;
+		next;
+	}
 	while($time){
 	    my $point=int(rand($gss)+1);
-	    if($id{$point} > 0){
-	        $id{$heads[$i]}{$point} --;
+	    if($id{$heads[$h]}{$point} > 0){
+	        $id{$heads[$h]}{$point} --;
 	        $time --;
 	    }
 	}
 
 	for my $i( 1 .. $gss) {
-	    $id{$heads[$i]}{$i} ||= 0;
-	    $abundance{$heads[$i]}{$i}=$id{$heads[$i]}{$i}/$lengths{$i};
-	    $totalabundance{$heads[$i]}+=$abundance{$heads[$i]}{$i};
-	    $totalreads{$heads[$i]}+=$id{$i};
+	    $id{$heads[$h]}{$i} ||= 0;
+	    $abundance{$heads[$h]}{$i}=$id{$heads[$h]}{$i}/$lengths{$i};
+	    $totalabundance{$heads[$h]}+=$abundance{$heads[$h]}{$i};
+#	    $totalreads{$heads[$i]}+=$id{$heads[$i]}{$i};
 	}
 }
 open OTR,">$out.reads.profile" or die "$out.profile $!\n";
 open OTA,">$out.abun.profile" or die "$out.profile $!\n";
 #open ST,">$out.stat" || die $!;
 #print OT "ID\treads_pairs\tbase_abundance\treads_abundance\n";
+$head = join("\t",@heads);
 print OTR $head."\n";
 print OTA $head."\n";
 
 for my $i(1 .. $gss) {
 	print OTR "$i";
 	print OTA "$i";
-	for my $h(1 .. @heads){
+	for my $h(1 .. $#heads){
+		my $abun = $abundance{$heads[$h]}{$i}/$totalabundance{$heads[$h]};
 #	    print OT "\t$id{$head[$h]}{$i}\t",$id{$head[$h]}{$i}/$totalreads{$head[$h]},"\t",$abundance{$head[$h]}{$i}/$totalabundance{$head[$h]};
 		print OTR "\t$id{$heads[$h]}{$i}";
-		print OTA "\t$abundance{$heads[$h]}{$i}/$totalabundance{$heads[$h]}";
+		print OTA "\t$abun";
 	}
 	print OTR "\n";
 	print OTA "\n";
 }
 close OTR;
 close OTA;
+close ST;
 #print ST "";
 #--------------------------------------------------------
 sub usage
