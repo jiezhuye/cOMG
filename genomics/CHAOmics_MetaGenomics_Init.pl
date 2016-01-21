ac2aee5f8557c552c69382492d03a00ef22be6f1
Scelta <scelta21st@gmail.com>
2016-01-21 11:16:56 +0800

    Added a linear script to run task under the order of sample (and u can still choose the former mode which run according to steps)

--- genomics/CHAOmics_MetaGenomics_Init.pl
+++ genomics/CHAOmics_MetaGenomics_Init.pl
@@ -93,13 +93,6 @@ system "mkdir -p $dir_c" unless(-d $dir_c or $step !~ /1/);
 system "mkdir -p $dir_r" unless(-d $dir_r or $step !~ /2/);
 system "mkdir -p $dir_sp" unless(-d $dir_sp or $step !~ /3/);
 
-#open B1,">$dir_s/batch.trim.sh";
-#open B2,">$dir_s/batch.filter.sh";
-open B1,">$dir_s/batch.clean.sh";
-open B2,">$dir_s/batch.rmhost.sh";
-open B3,">$dir_s/batch.soap.sh";
-open C1,">$out_dir/qsub_all.sh";
-
 open IN,"<$path_f" || die $!;
 my (%SAM,@samples,$tmp_out,$tmp_outN,$tmp_outQ);
 while (<IN>){
@@ -108,61 +101,7 @@ while (<IN>){
 	my ($sam,$pfx,$path) = @a;
 	$SAM{$sam}{$pfx} = $path;
 }
-
-foreach my $sam (sort keys %SAM){ my @FQS = sort keys %{$SAM{$sam}}; while(@FQS >0){
-
-	my @fqs = ($pattern eq "pe")?(shift @FQS, shift @FQS):(shift @FQS);
-	my $fq1 = $SAM{$sam}{$fqs[0]};
-	my $fq2 = $SAM{$sam}{$fqs[1]}||die "miss fq2 under pe pattern. $!\n" if @fqs eq 2;
 ###############################
-	if ($step =~ /1/){
-		open SHC,">$dir_sI/$sam.clean.sh";
-		my $seq = "";
-		if (@fqs eq 2){
-			$seq = "$fq1,$fq2";
-			($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_c/$sam.clean.fq1.gz","$dir_c/$sam.clean.fq2.gz");
-		}else{
-			$seq = $fq1;
-			$tmp_out = "$dir_c/$sam.clean.fq.gz";
-		}
-		print SHC "perl $s_clean $seq $dir_c/$sam $CFG{'Qt'} $CFG{'l'} $CFG{'N'} $CFG{'Qf'} $CFG{'lf'}\n";
-#		close SH;
-#		print B1 "sh $dir_sI/$sam.clean.sh\n";
-	}
-###############################
-	if ($step =~ /2/){
-		open SHR,">$dir_sI/$sam.rmhost.sh";
-		my $seq = "";
-		if (@fqs eq 2){
-			$seq = "-a $SAM{$sam}{$fqs[0]} -b $SAM{$sam}{$fqs[1]}";
-			($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_r/$sam.rmhost.1.fq.gz","$dir_r/$sam.rmhost.2.fq.gz");
-		}else{
-			$seq = "-a $tmp_out";
-			$tmp_out = "$dir_r/$sam.rmhost.fq.gz";
-		}
-		print SHR "perl $s_rm $seq -d $s_db -m 4 -s 32 -s 30 -r 1 -v 7 -i 0.9 -t 8 -f Y -p  $dir_r/$sam -q\n";
-#		close SH;
-#		print B2 "sh $dir_sI/$sam.rmhost.sh\n";
-	}
-###############################
-	if ($step =~ /3/){
-		my $seq = "";
-		open SHS,">$dir_sI/$sam.soap.sh";
-		if (@fqs eq 2){
-			$seq = "-i1 $SAM{$sam}{$fqs[0]} -i2 $SAM{$sam}{$fqs[1]}";
-		}else{
-			$seq = "-i1 $tmp_out";
-		}
-		print SHS "perl $s_soap $seq -ins $ins_f -o $dir_s -p $sam > $dir_sp/$sam.log\n";
-#		close SH;
-#		print B3 "sh $dir_sI/$sam.soap.sh\n";
-	}
-}
-close SHC;close SHR;close SHS;
-print B1 "sh $dir_sI/$sam.clean.sh\n";
-print B2 "sh $dir_sI/$sam.rmhost.sh\n";
-print B3 "sh $dir_sI/$sam.soap.sh\n";
-}
 $CFG{'q'}  ||= "st.q";
 $CFG{'P'}  ||= "st_ms";
 $CFG{'pro'}  ||= 8;
@@ -170,15 +109,101 @@ $CFG{'vf1'} ||= "0.3G";
 $CFG{'vf2'} ||= "8G";
 $CFG{'vf3'} ||= "15G";
 $CFG{'m'} ||= 30;
-#alias compute2416="ssh compute-24-16"
+
+## start <- top exec batch scripts
+open C1,">$out_dir/qsub_all.sh";
 print C1 "perl /home/fangchao/bin/qsub_all.pl -N B.c -d $dir_s/qsub_1 -l vf=$CFG{'vf1'} -q $CFG{'q'} -P $CFG{'P'} -r -m $CFG{'m'} $dir_s/batch.clean.sh\n" if $step =~ /1/;
 print C1 "perl /home/fangchao/bin/qsub_all.pl -N B.r -d $dir_s/qsub_2 -l vf=$CFG{'vf2'},p=$CFG{'pro'} -q $CFG{'q'} -P $CFG{'P'} -r -m $CFG{'m'} $dir_s/batch.rmhost.sh\n" if $step =~ /2/;
 print C1 "perl /home/fangchao/bin/qsub_all.pl -N B.s -d $dir_s/qsub_3 -l vf=$CFG{'vf3'},p=$CFG{'pro'} -q $CFG{'q'} -P $CFG{'P'} -r -m $CFG{'m'} $dir_s/batch.soap.sh\n" if $step =~ /3/;
+close C1;
+## done! <- top exec batch scripts
+#
+## start <- contents of each batch scripts
+open C2,">$out_dir/linear.$step.sh"; 
+open B1,">$dir_s/batch.clean.sh";
+open B2,">$dir_s/batch.rmhost.sh";
+open B3,">$dir_s/batch.soap.sh";
+###############################
+foreach my $sam (sort keys %SAM){ 
+	### Write main batch scripts first.
+	open LINE,"> $dir_sL/$sam.$step.sh";
+	if ($step =~ /1/){
+		open SHC,">$dir_sI/$sam.clean.sh";
+		print LINE "perl /home/fangchao/bin/qsub_all.pl -N B.c -d $dir_s/qsub_1 -l vf=$CFG{'vf1'} -q $CFG{'q'} -P $CFG{'P'} -r -m $CFG{'m'} $dir_sI/$sam.clean.sh\n";
+		print B1 "sh $dir_sI/$sam.clean.sh\n";
+	}
+	if ($step =~ /2/){
+		open SHR,">$dir_sI/$sam.rmhost.sh";
+		print LINE "perl /home/fangchao/bin/qsub_all.pl -N B.r -d $dir_s/qsub_2 -l vf=$CFG{'vf2'},p=$CFG{'pro'} -q $CFG{'q'} -P $CFG{'P'} -r -m $CFG{'m'} $dir_s/$sam.rmhost.sh\n";
+		print B2 "sh $dir_sI/$sam.rmhost.sh\n";
+	}
+	if ($step =~ /3/){
+		open SHS,">$dir_sI/$sam.soap.sh";
+		print LINE "perl /home/fangchao/bin/qsub_all.pl -N B.s -d $dir_s/qsub_3 -l vf=$CFG{'vf3'},p=$CFG{'pro'} -q $CFG{'q'} -P $CFG{'P'} -r -m $CFG{'m'} $dir_s/$sam.soap.sh\n";
+		print B3 "sh $dir_sI/$sam.soap.sh\n";
+	}
+	close LINE;
+	print C2 "sh $dir_sL/$sam.$step.sh \&\n";
 
+	my @FQS = sort keys %{$SAM{$sam}};
+	# U now under the sample level ( biological meaning )
+	# below loop is under the data level ( basic unit to do the data mining )
+	while(@FQS >0){
+
+		my @fqs = ($pattern eq "pe")?(shift @FQS, shift @FQS):(shift @FQS);
+		my $fq1 = $SAM{$sam}{$fqs[0]};
+		my $fq2 = $SAM{$sam}{$fqs[1]}||die "miss fq2 under pe pattern. $!\n" if @fqs eq 2;
+###############################
+		if ($step =~ /1/){
+#		open SHC,">$dir_sI/$sam.clean.sh";
+			my $seq = "";
+			if (@fqs eq 2){
+				$seq = "$fq1,$fq2";
+				($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_c/$sam.clean.fq1.gz","$dir_c/$sam.clean.fq2.gz");
+			}else{
+				$seq = $fq1;
+				$tmp_out = "$dir_c/$sam.clean.fq.gz";
+			}
+			print SHC "perl $s_clean $seq $dir_c/$sam $CFG{'Qt'} $CFG{'l'} $CFG{'N'} $CFG{'Qf'} $CFG{'lf'}\n";
+#		close SH;
+#		print B1 "sh $dir_sI/$sam.clean.sh\n";
+		}
+###############################
+		if ($step =~ /2/){
+#		open SHR,">$dir_sI/$sam.rmhost.sh";
+			my $seq = "";
+			if (@fqs eq 2){
+				$seq = "-a $SAM{$sam}{$fqs[0]} -b $SAM{$sam}{$fqs[1]}";
+				($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_r/$sam.rmhost.1.fq.gz","$dir_r/$sam.rmhost.2.fq.gz");
+			}else{
+				$seq = "-a $tmp_out";
+				$tmp_out = "$dir_r/$sam.rmhost.fq.gz";
+			}
+			print SHR "perl $s_rm $seq -d $s_db -m 4 -s 32 -s 30 -r 1 -v 7 -i 0.9 -t 8 -f Y -p  $dir_r/$sam -q\n";
+#		close SH;
+#		print B2 "sh $dir_sI/$sam.rmhost.sh\n";
+		}
+###############################
+		if ($step =~ /3/){
+			my $seq = "";
+#		open SHS,">$dir_sI/$sam.soap.sh";
+			if (@fqs eq 2){
+				$seq = "-i1 $SAM{$sam}{$fqs[0]} -i2 $SAM{$sam}{$fqs[1]}";
+			}else{
+				$seq = "-i1 $tmp_out";
+			}
+			print SHS "perl $s_soap $seq -ins $ins_f -o $dir_s -p $sam > $dir_sp/$sam.log\n";
+#		close SH;
+#		print B3 "sh $dir_sI/$sam.soap.sh\n";
+		}
+	}
+	close SHC;close SHR;close SHS;
+}
 close B1;
 close B2;
 close B3;
-
+close C2;
+## done! <- contents of each batch scripts
 
 # ####################
 # SUB FUNCTION
