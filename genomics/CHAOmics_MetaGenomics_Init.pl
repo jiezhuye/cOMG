2b058608fc3cd44e39b4bf86d2ac519bfe8846e7
Scelta <scelta21st@gmail.com>
2016-01-21 14:37:15 +0800

    Adjust for such situation: once a specific sample was sequenced several times. I'd better recorded them together before the abundance building.

--- genomics/CHAOmics_MetaGenomics_Init.pl
+++ genomics/CHAOmics_MetaGenomics_Init.pl
@@ -70,6 +70,7 @@ my $bin = "$Bin/bin";
 my $s_clean  = "$bin/readsCleaning.pl";
 my $s_rm     = "/ifs5/PC_MICRO_META/PRJ/MetaSystem/analysis_flow/bin/program/rmhost_v1.0.pl";
 my $s_soap   = "$bin/soap2BuildAbundance.dev.pl";
+my $s_abun   = "$bin/BuildAbundance.dev.pl";
 # public database prefix
 my $s_db     = "/nas/RD_09C/resequencing/resequencing/tmp/pub/Genome/Human/human.fa.index";
 # project results directiory structure
@@ -124,7 +125,7 @@ open B1,">$dir_s/batch.clean.sh";
 open B2,">$dir_s/batch.rmhost.sh";
 open B3,">$dir_s/batch.soap.sh";
 ###############################
-foreach my $sam (sort keys %SAM){ 
+foreach my $sam (sort keys %SAM){ # operation on sample level
 	### Write main batch scripts first.
 	open LINE,"> $dir_sL/$sam.$step.sh";
 	if ($step =~ /1/){
@@ -139,32 +140,41 @@ foreach my $sam (sort keys %SAM){
 	}
 	if ($step =~ /3/){
 		open SHS,">$dir_sI/$sam.soap.sh";
+		open LIST,">$dir_sp/$sam.soap.list";
 		print LINE "perl /home/fangchao/bin/qsub_all.pl -N B.s -d $dir_s/qsub_3 -l vf=$CFG{'vf3'},p=$CFG{'pro'} -q $CFG{'q'} -P $CFG{'P'} -r -m $CFG{'m'} $dir_s/$sam.soap.sh\n";
 		print B3 "sh $dir_sI/$sam.soap.sh\n";
+		print B3 "sh $dir_sI/$sam.abun.sh\n";
 	}
 	close LINE;
 	print C2 "sh $dir_sL/$sam.$step.sh \&\n";
 
+	my $list ="";
 	my @FQS = sort keys %{$SAM{$sam}};
 	# U now under the sample level ( biological meaning )
 	# below loop is under the data level ( basic unit to do the data mining )
-	while(@FQS >0){
-
+	while(@FQS >0){ # Fastqs processed under this loop
 		my @fqs = ($pattern eq "pe")?(shift @FQS, shift @FQS):(shift @FQS);
 		my $fq1 = $SAM{$sam}{$fqs[0]};
 		my $fq2 = $SAM{$sam}{$fqs[1]}||die "miss fq2 under pe pattern. $!\n" if @fqs eq 2;
+		my $pfx = $fq1;
+		if (@fqs eq 2){
+			my @a = $fqs[0] =~/^(\S+)([.-_])([12ab])$/;
+			my @b = $fqs[1] =~/^(\S+)([.-_])([12ab])$/;
+			die "Does $fqs[0] and $fqs[1] seems not belong to a pair fq? Make sure they got same string before [.-_][12ab]\n" if $a[0] ne $b[0];
+			$pfx = $a[0];
+		}
 ###############################
 		if ($step =~ /1/){
 #		open SHC,">$dir_sI/$sam.clean.sh";
 			my $seq = "";
 			if (@fqs eq 2){
 				$seq = "$fq1,$fq2";
-				($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_c/$sam.clean.fq1.gz","$dir_c/$sam.clean.fq2.gz");
+				($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_c/$pfx.clean.fq1.gz","$dir_c/$pfx.clean.fq2.gz");
 			}else{
 				$seq = $fq1;
-				$tmp_out = "$dir_c/$sam.clean.fq.gz";
+				$tmp_out = "$dir_c/$pfx.clean.fq.gz";
 			}
-			print SHC "perl $s_clean $seq $dir_c/$sam $CFG{'Qt'} $CFG{'l'} $CFG{'N'} $CFG{'Qf'} $CFG{'lf'}\n";
+			print SHC "perl $s_clean $seq $dir_c/$pfx $CFG{'Qt'} $CFG{'l'} $CFG{'N'} $CFG{'Qf'} $CFG{'lf'}\n";
 #		close SH;
 #		print B1 "sh $dir_sI/$sam.clean.sh\n";
 		}
@@ -174,12 +184,12 @@ foreach my $sam (sort keys %SAM){
 			my $seq = "";
 			if (@fqs eq 2){
 				$seq = "-a $SAM{$sam}{$fqs[0]} -b $SAM{$sam}{$fqs[1]}";
-				($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_r/$sam.rmhost.1.fq.gz","$dir_r/$sam.rmhost.2.fq.gz");
+				($SAM{$sam}{$fqs[0]}, $SAM{$sam}{$fqs[1]}) = ("$dir_r/$pfx.rmhost.1.fq.gz","$dir_r/$pfx.rmhost.2.fq.gz");
 			}else{
 				$seq = "-a $tmp_out";
-				$tmp_out = "$dir_r/$sam.rmhost.fq.gz";
+				$tmp_out = "$dir_r/$pfx.rmhost.fq.gz";
 			}
-			print SHR "perl $s_rm $seq -d $s_db -m 4 -s 32 -s 30 -r 1 -v 7 -i 0.9 -t 8 -f Y -p  $dir_r/$sam -q\n";
+			print SHR "perl $s_rm $seq -d $s_db -m 4 -s 32 -s 30 -r 1 -v 7 -i 0.9 -t 8 -f Y -p  $dir_r/$pfx -q\n";
 #		close SH;
 #		print B2 "sh $dir_sI/$sam.rmhost.sh\n";
 		}
@@ -189,15 +199,24 @@ foreach my $sam (sort keys %SAM){
 #		open SHS,">$dir_sI/$sam.soap.sh";
 			if (@fqs eq 2){
 				$seq = "-i1 $SAM{$sam}{$fqs[0]} -i2 $SAM{$sam}{$fqs[1]}";
+				$list .="$dir_sp/$sam.gene.build/$pfx.soap.pair.pe\n";
 			}else{
 				$seq = "-i1 $tmp_out";
 			}
-			print SHS "perl $s_soap $seq -ins $ins_f -o $dir_s -p $sam > $dir_sp/$sam.log\n";
+			print SHS "perl $s_soap $seq -o $dir_sp -s $sam -p $pfx > $dir_sp/$pfx.log\n";
+			$list .="$dir_sp/$sam.gene.build/$pfx.soap.pair.se\n";
 #		close SH;
 #		print B3 "sh $dir_sI/$sam.soap.sh\n";
 		}
 	}
 	close SHC;close SHR;close SHS;
+	if ($step =~ /3/){ # Since step3 contains abundance building which needs operated on sample level, I've got to put them here.
+		print LIST $list;
+		close LIST;
+		open ABUN,">$dir_sI/$sam.abun.sh";
+		print ABUN "perl $s_abun -ins $ins_f -o $dir_sp -p $sam\n";
+		close ABUN;
+	}
 }
 close B1;
 close B2;
