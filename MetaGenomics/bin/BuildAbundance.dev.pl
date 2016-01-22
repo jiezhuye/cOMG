8ae3b62fd1c022b6d481bf571298a0d749260b9f
Scelta <scelta21st@gmail.com>
2016-01-22 23:24:37 +0800

    remove annotation

--- MetaGenomics/bin/BuildAbundance.dev.pl
+++ MetaGenomics/bin/BuildAbundance.dev.pl
@@ -6,12 +6,6 @@ use FindBin qw/$Bin/;
 
 my ($ab,$ins,$list,$prefix,$help,$workpath);
 GetOptions(
-        #"s:s"=>\$scaf_fa,
-	#"g:s"=>\$gene_prediction_fa,
-#	"i1:s"=>\$sample_pair_1,
-#	"i2:s"=>\$sample_pair_2,
-#	"i3:s"=>\$sample_single,
-#	"par:s"=>\$parameter,
 	"ab:i" =>\$ab,
 	"ins:s"=>\$ins,
 	"l:s"=>\$list,
@@ -20,53 +14,10 @@ GetOptions(
 	"h:s"=>\$help,
         );
 
-#do{&usage;exit(1);} if ($help || !defined($prefix ||$sample_pair_1) );
-
-#$parameter ||= ",r=0,l=30,M=4,S,p=8,v=5,S,c=0.95";
-#$parameter =~ s/,/ -/g;
-#$parameter =~ s/=/ /g;
-
 $ab ||= 123;
 chomp (my $pwd=`pwd`);
 $workpath||=$pwd;
 
-####SOAP####
-=cut
-my $soap_path = "/ifs1/ST_MD/USER/chenwn/bin/profiling/bin/soap2.22";
-my $db_index1 = "/ifs1/ST_MD/USER/caixianghang/backup/MetaHit/27.1267sample_profile/list/db/4Group_uniqGene.div_1.fa.index";
-my $db_index2 = "/ifs1/ST_MD/USER/caixianghang/backup/MetaHit/27.1267sample_profile/list/db/4Group_uniqGene.div_2.fa.index";
-####conf####
-`mkdir -p $workpath/$prefix.gene.build`;
-
-if($sample_single){
-	my $cmd = "$soap_path -a $sample_pair_1 -b $sample_pair_2 -D $db_index1 -D $db_index2 -o $workpath/$prefix.gene.build/$prefix.soap.pair.pe -2 $workpath/$prefix.gene.build/$prefix.soap.pair.se $parameter 2> $workpath/$prefix.gene.build/$prefix.soap.pair.log\n";
-	print STDERR "$cmd\n";
-	`$cmd`;print STDERR "PE soap finished!\n";
-	$cmd = "$soap_path -a $sample_single -D $db_index1 -D $db_index2 -o $workpath/$prefix.gene.build/$prefix.soap.single.se $parameter 2>$workpath/$prefix.gene.build/$prefix.soap.single.log\n";
-	 print STDERR "$cmd";
-	`$cmd`;print STDERR "SE soap finished!\n";
-}els
-
-if($sample_pair_2){
-	my $cmd = "$soap_path -a $sample_pair_1 -b $sample_pair_2 -D $db_index1 -D $db_index2 -m 226 -x 426 -o $workpath/$prefix.gene.build/$prefix.soap.pair.pe -2 $workpath/$prefix.gene.build/$prefix.soap.pair.se $parameter 2> $workpath/$prefix.gene.build/$prefix.soap.pair.log\n";
-	print STDERR "$cmd";
-	`$cmd`;print STDERR "PE soap finished!\n";
-}elsif($sample_pair_1){
-	my $cmd = "$soap_path -a $sample_pair_1 -D $db_index1 -D $db_index2 -m 226 -x 426 -o $workpath/$prefix.gene.build/$prefix.soap.pair.pe -2 $workpath/$prefix.gene.build/$prefix.soap.pair.se $parameter 2> $workpath/$prefix.gene.build/$prefix.soap.pair.log\n";
-	print STDERR "$cmd";
-	`$cmd`;print STDERR "SE soap finished!\n";
-}
-
-chomp(my $f = `ls $workpath/$prefix.gene.build/*.[ps]e|head -1`);
-chomp(my $g = `ls $workpath/$prefix.gene.build/*.[ps]e.gz|head -1`);
-if (-e $f){
-	`find $workpath/$prefix.gene.build/*.[ps]e >$workpath/$prefix.soap.list`;
-}elsif(-e $g){
-	`find $workpath/$prefix.gene.build/*gz > $workpath/$prefix.soap.list`;
-}else{
-	die "can not find soap result files.".$!;
-}
-=cut
 if ($ab =~ /1/){
 	my $cmd = "perl /ifs1/ST_MD/USER/chenwn/bin/profiling/bin/gene_Profiling.pl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/list/760MetaHit_139HMP_368PKU_511Bac.uniq.fa.len $ins $list $workpath/$prefix\n";
 	print STDERR $cmd;`$cmd`;
@@ -84,7 +35,6 @@ if ($ab =~ /3/){
 }
 
 ####### compress the results to save space ### added by fangchao@genomics.cn
-#`gzip $workpath/$prefix.gene.build/*.[ps]e`;
 `gzip -f $workpath/$prefix*.abundance`;
 
 sub usage {
