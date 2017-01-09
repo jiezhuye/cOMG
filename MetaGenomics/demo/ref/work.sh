a67a10e25ff205cff09b8fdf3cadcb5772cc1b1c
Fangchao <fangchao@genomics.cn>
2017-01-09 12:19:06 +0800

    make demo reference for test.

---
+++ MetaGenomics/demo/ref/work.sh
@@ -0,0 +1,5 @@
+# some basic tools required, you may add the our Binboard dir into your PATH:
+# export PATH=$PATH:/ifs1/ST_MD/PMO/script/bin/
+
+#zcat $MLIB/760MetaHIT_139HMP_368PKU_511Bac_IGC.fa.gz|perl -e '$/=">";<>;for(1..100){$_=<>;chomp;print ">$_"}' > IGC.100.demo.fa
+2bwt-builder IGC.100.demo.fa
