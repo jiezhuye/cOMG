cba81fa712f7fcc0396cbf00672104c8d76b01e3
Fangchao <fangchao@genomics.cn>
2017-01-10 14:07:48 +0800

    make ref script.

--- MetaGenomics/demo/ref/work.sh
+++ MetaGenomics/demo/ref/work.sh
@@ -1,5 +1,9 @@
 # some basic tools required, you may add the our Binboard dir into your PATH:
 # export PATH=$PATH:/ifs1/ST_MD/PMO/script/bin/
 
+# make ref demo (100 gene)
 #zcat $MLIB/760MetaHIT_139HMP_368PKU_511Bac_IGC.fa.gz|perl -e '$/=">";<>;for(1..100){$_=<>;chomp;print ">$_"}' > IGC.100.demo.fa
+# make index
 2bwt-builder IGC.100.demo.fa
+
+grep ">" IGC.100.demo.fa|sed 's/>//'|awk '{print $1}' > gene.name.lst
