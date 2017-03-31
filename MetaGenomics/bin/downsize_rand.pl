713fd1da79dc993209546dd7547ce3eb2af72a1f
Fangchao <fangchao@genomics.cn>
2017-03-31 17:20:58 +0800

    remove dowsize script bug version and add one work correct

--- MetaGenomics/bin/downsize_rand.pl
+++ MetaGenomics/bin/downsize_rand.pl
@@ -1,28 +1,27 @@
 #!/usr/bin/perl -w
 use strict;
 die &usage unless @ARGV >= 4;
-my($targetSize,$reads,$length,$out,$single_double) = @ARGV;
+my($originSize,$targetSize,$reads,$length,$out,$single_double) = @ARGV;
 # $single_double means weather the reads counts need to be counted tiwce (because some pair-end abundance calculate 1 read as half a *pair*)
 $single_double ||= 1;
 sub openMethod{my $f=shift;return((($f =~ /\.gz$/)?"gzip -dc $f|":"$f"))}
+sub writeM{my $f=shift;return((($f =~ /\.gz$/)?"|gzip > $f":"> $f"))}
 ################################## downsize ######################################
-
-my $gss = 0; # gene set size of $reads
+srand($originSize);
 my (%reads_num,@rands);
 open I,&openMethod($reads) or die "$!\n";
-open ST, ">$out.abundance";
-my $total_reads=0;
+open ST, &writeM($out) or die "$!\n";
+my $total_reads=$originSize;
 my $k=0;
 while(<I>)
 {
     chomp;
     my @temp=split;
 	next if $.==1 && $temp[1]=~/[a-z]/;
- 	$gss ++;
-	$total_reads+= $temp[1] * $single_double ;
-    $reads_num{$temp[0]}=$temp[1] * $single_double;
+	$total_reads+= $temp[1] * $single_double if $originSize==0;
+    $reads_num{$temp[0]}=($originSize-2*$targetSize<0)?$temp[1] * $single_double:0;
 	if ($temp[1] >0){
-		my $t=$temp[1];
+		my $t=$temp[1] * $single_double;
 		while($t){
 			$rands[$k] = $temp[0];
 			$k ++;$t --;
@@ -36,9 +35,11 @@ if ($time < 0){
 	print ST "Less than $targetSize ($total_reads). Discarded.\n";
 	exit;
 }else{
+	$time = ($time < $targetSize)?$time:$targetSize;
+	my $one  = ($time < $targetSize)?-1:1;
 	while($time){
 	  	my $point=int(rand(@rands));
-	    $reads_num{$rands[$point]} --;
+	    $reads_num{$rands[$point]} += $one;
 		splice(@rands,$point,1);
 	    $time --;
 	}
@@ -47,7 +48,7 @@ if ($time < 0){
 
 ################################ abundance ################################
 
-open I,"$length" or die "$!\n";
+open I,&openMethod($length) or die "$!\n";
 my $gene_n=0;###size of gene length profile
 my %reads_abundance;### reads_abundance=reads number/gene length
 my $total_abundance=0;### total_abundance of reads_abundance
@@ -75,7 +76,8 @@ for my $i(1 .. $gene_n) {
 close ST;
 sub usage
 {
-    print "usage:perl $0 [targetSize][reads table][len.info][output prefix]\n
+    print "usage:perl $0 [origin read number] [targetSize] [input file] [len.info file] [output ifle]\n
+			The number of reads input contain. [0] if unknown
 			The number of reads you wanna trim.
 			A list contain the gene id and reads number(gene & reads number before downsize)
 			A list contain the gene id and its length(gene length reference)
